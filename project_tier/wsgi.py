06054f4c8c14742dc7d25ee5149f561424130631
Matt McManus <matt@mcmanus.io>
2016-01-07 15:43:19 -0500

    Tiding up heroku setup

--- project_tier/wsgi.py
+++ project_tier/wsgi.py
@@ -6,15 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
 
-from whitenoise.django import DjangoWhiteNoise
-
-from django.core.wsgi import get_wsgi_application
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_tier.settings.production")
+
+from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
