b0404149ae43b699eb6858fc00aa67beeefa565b
Matt McManus <matt@mcmanus.io>
2016-01-07 15:43:19 -0500

    Tiding up heroku setup

--- project_tier/heroku_wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_tier.settings.production")
-
-application = Cling(get_wsgi_application())
