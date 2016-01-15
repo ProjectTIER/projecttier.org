14ce32a382e01eeba2e20490362a813c0f0a9300
Matt McManus <matt@mcmanus.io>
2016-01-15 10:27:59 -0500

    Fix some lingering tests

--- project_tier/protocol/tests.py
+++ project_tier/protocol/tests.py
@@ -9,8 +9,29 @@ from project_tier.protocol.models import (ComponentIndexPage, ComponentPage,
 
 class TestProtocolPageTests(WagtailPageTests):
     def test_assert_allowed_parent_page_types(self):
-        self.assertAllowedParentPageTypes(ComponentPage, {ComponentIndexPage})
+        self.assertAllowedParentPageTypes(ComponentPage, {ComponentIndexPage, ComponentPage})
         self.assertAllowedParentPageTypes(ComponentIndexPage, {ProtocolHomePage})
     #
     # def test_assert_allowed_subpage_types(self):
     #     self.assertAllowedSubpageTypes(ProtocolHomePage, {ComponentIndexPage, StandardPage})
+
+class ComponentIndexPageFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = ComponentIndexPage
+    title = "Components"
+
+class ComponentPageFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = ComponentPage
+    title = "Component"
+
+class TestComponentPage(TestCase):
+  def setUp(self):
+    self.root = ComponentIndexPage.add_root(instance=ComponentIndexPageFactory.build())
+    self.root.add_child(instance=ComponentPageFactory.build())
+
+  def tearDown(self):
+    self.root.delete()
+
+  def test_person_page_created(self):
+    nt.eq_(self.root.title, "Components")
