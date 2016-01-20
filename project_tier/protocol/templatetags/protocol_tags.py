22b52f1c099bc24b02033776bd94584d1ffe66b7
Matt McManus <matt@mcmanus.io>
2016-01-20 15:43:30 -0500

    Properly detect subpages in protocol_menu

--- project_tier/protocol/templatetags/protocol_tags.py
+++ project_tier/protocol/templatetags/protocol_tags.py
@@ -1,7 +1,8 @@
 from django import template
 from django.conf import settings
 
-from project_tier.protocol.models import (ProtocolHomePage, ComponentIndexPage, ComponentPage)
+from project_tier.protocol.models import (ProtocolProcessPage, ProtocolHomePage,
+                                         ComponentIndexPage, ComponentPage)
 
 register = template.Library()
 
@@ -11,19 +12,22 @@ def get_site_root(context):
     # so object-comparison to self will return false as objects would differ
     return context['request'].site.root_page
 
-
-
 def has_children(page):
     return page.get_children().live().exists()
+
+def has_children_in_menu(page):
+    return page.get_children().live().in_menu().exists()
+
+def is_active(page, current_page):
+    return (current_page.url.startswith(page.url) if current_page else False)
 
 @register.inclusion_tag('tags/protocol_menu.html', takes_context=True)
 def protocol_menu(context, calling_page=None):
     menuitems = calling_page.get_ancestors().type(ProtocolHomePage).last().get_children().in_menu()
 
     for menuitem in menuitems:
-        menuitem.active = (calling_page.url.startswith(menuitem.url)
-                           if calling_page else False)
-        menuitem.has_children = has_children(menuitem)
+        menuitem.is_active = is_active(menuitem, calling_page)
+        menuitem.has_children = has_children_in_menu(menuitem)
 
         if menuitem.has_children:
             menuitem.children = menuitem.get_children().live().order_by('title')
@@ -53,8 +57,7 @@ def component_menu_item(context, item, calling_page=None):
     # Not sure item is just a Page instead of a ComponentPage. I need it's type
     item = ComponentPage.objects.get(pk=item.id)
     item.has_children = has_children(item)
-    item.is_active = (calling_page.url.startswith(item.url)
-                           if calling_page else False)
+    item.is_active = is_active(item, calling_page)
 
     menuitems_children = item.get_children().live().order_by('title')
 
