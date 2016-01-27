200f464a847fae9c92ec018e0f1a596678648e0f
Matt McManus <matt@mcmanus.io>
2016-01-27 11:55:30 -0500

    Email matt for errors in prod

--- project_tier/settings/production.py
+++ project_tier/settings/production.py
@@ -10,17 +10,6 @@ from .base import *
 DEBUG = False
 TEMPLATES[0]['OPTIONS']['debug'] = False
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# Compress static files offline and minify CSS
-# http://django-compressor.readthedocs.org/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-COMPRESS_CSS_HASHING_METHOD = 'content'
-
-
 # Configuration from environment variables
 # Alternatively, you can set these in a local.py file on the server
 
@@ -60,6 +49,16 @@ if 'CACHE_PURGE_URL' in env:
             'LOCATION': env['CACHE_PURGE_URL'],
         },
     }
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Compress static files offline and minify CSS
+# http://django-compressor.readthedocs.org/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
 
 if 'STATIC_URL' in env:
     STATIC_URL = env['STATIC_URL']
