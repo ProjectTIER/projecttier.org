6a77ca48f7433036f0ad461c2175d3b86e649fbc
Andrew Croce <andrew.croce@gmail.com>
2016-01-29 09:37:25 -0500

    merge migrations

---
+++ project_tier/home/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0019_auto_20160128_1220'),
+        ('home', '0020_personindexpage_body'),
+    ]
+
+    operations = [
+    ]
