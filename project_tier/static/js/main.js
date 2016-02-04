c0ed0bbaa73d5bf1080d28e4dc7c9e0fc81dfa8e
Andrew Croce <andrew.croce@gmail.com>
2016-02-04 16:50:48 -0500

    Check rich text blocks for links, and if there are matching hrefs in a component meant, highlight them

--- project_tier/static/js/main.js
+++ project_tier/static/js/main.js
@@ -9,3 +9,25 @@ $(window).on('scroll', Foundation.util.throttle(function(e){
   }
 
 }, 300));
+
+
+(function($){
+
+  $(document).ready(function(){
+
+    /*
+    * Component links inside rich text will automatically hightlight any matching component nav items 
+    */
+    $('.rich-text a').each(function(){
+      var href = $(this).attr('href');
+      var components = $('.component-nav a[href="'+href+'"]').addClass('highlight');
+      $(this).hover(
+        function(){
+          components.addClass('active');
+        }, function(){
+          components.removeClass('active');
+        });
+    });
+  });
+
+})(jQuery);
