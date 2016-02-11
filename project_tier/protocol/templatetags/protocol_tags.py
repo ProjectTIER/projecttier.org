93da1d7946fb5d2277929f3b3aa928e2029d9775
Andrew Croce <andrew.croce@gmail.com>
2016-02-11 08:29:13 -0500

    Differentiate `is_active()` from new function `is_active_tree()` so we can distinguish if a full page hierarchy is active, or just a single page

--- project_tier/protocol/templatetags/protocol_tags.py
+++ project_tier/protocol/templatetags/protocol_tags.py
@@ -20,6 +20,9 @@ def has_children_in_menu(page):
     return page.get_children().live().in_menu().exists()
 
 def is_active(page, current_page):
+    return (current_page.url == page.url if current_page else False)
+
+def is_active_tree(page, current_page):
     return (current_page.url.startswith(page.url) if current_page else False)
 
 
@@ -28,7 +31,7 @@ def protocol_menu(context, calling_page=None):
     menuitems = calling_page.get_ancestors().type(ProtocolHomePage).last().get_children().in_menu()
 
     for menuitem in menuitems:
-        menuitem.is_active = is_active(menuitem, calling_page)
+        menuitem.is_active = is_active_tree(menuitem, calling_page)
         menuitem.has_children = has_children_in_menu(menuitem)
 
         if menuitem.has_children:
