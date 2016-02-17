f8fb635d66e2d24a64a9c35514f63282f05bcef9
Matt McManus <matt@mcmanus.io>
2016-02-17 15:58:26 -0500

    People list now has easy groups and secondary pages

--- project_tier/home/tests.py
+++ project_tier/home/tests.py
@@ -31,7 +31,7 @@ class TestPersonPageCreation(TestCase):
 class TestPageTests(WagtailPageTests):
     def test_assert_allowed_parent_page_types(self):
         self.assertAllowedParentPageTypes(EventIndexPage, {HomePage, StandardPage})
-        self.assertAllowedParentPageTypes(PersonIndexPage, {HomePage, StandardPage})
+        self.assertAllowedParentPageTypes(PersonIndexPage, {HomePage, StandardPage, PersonIndexPage})
 
         self.assertAllowedParentPageTypes(EventPage, {EventIndexPage})
         self.assertAllowedParentPageTypes(PersonPage, {PersonIndexPage})
