1850afd560ee9e4caf8553513b9404434655f576
Edd Baldry <edd.baldry@torchbox.com>
2016-07-28 13:48:43 +0100

    Reduce range of years for for fellows.

--- project_tier/people/models.py
+++ project_tier/people/models.py
@@ -87,7 +87,7 @@ class PersonPage(Page):
 
 class FellowPersonPage(PersonPage):
     YEAR_CHOICES = []
-    for r in range(1980, (datetime.datetime.now().year+1)):
+    for r in range(2010, (datetime.datetime.now().year+1)):
         YEAR_CHOICES.append((r, r))
 
     fellowship_year = models.IntegerField(
