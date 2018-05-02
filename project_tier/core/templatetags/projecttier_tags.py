cbe6431d8f3a4c63ff1d6497ec19e292c878ba4c
Alex Gleason <alex@alexgleason.me>
2018-05-02 18:49:23 -0400

    Switch to Google Docs for embedding exercise PDFs

--- project_tier/core/templatetags/projecttier_tags.py
+++ project_tier/core/templatetags/projecttier_tags.py
@@ -7,3 +7,10 @@ register = template.Library()
 @register.filter
 def document_view_url(value):
     return reverse('view_document', args=[value.id, value.filename])
+
+@register.simple_tag(takes_context=True)
+def pdfview(context, value):
+    embed_url = "https://docs.google.com/gview?url={}&embedded=true"
+    site_url = context.request.build_absolute_uri('/')[0:-1]
+    pdf_url = document_view_url(value)
+    return embed_url.format("{}{}".format(site_url, pdf_url))
