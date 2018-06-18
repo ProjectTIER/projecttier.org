332c1c8eaeb0dad5ce85c42a4d37ba67f18f6c44
Alex Gleason <alex@alexgleason.me>
2018-06-18 18:31:23 -0400

    Enable analytics only when explicitly configured to

--- project_tier/core/templatetags/projecttier_tags.py
+++ project_tier/core/templatetags/projecttier_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.urls import reverse
+from django.conf import settings
 
 register = template.Library()
 
@@ -7,3 +8,8 @@ register = template.Library()
 @register.filter
 def document_view_url(value):
     return reverse('view_document', args=[value.id, value.filename])
+
+
+@register.simple_tag
+def get_setting(name):
+    return getattr(settings, name, "")
