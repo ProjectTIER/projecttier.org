724ed3de20ec9ca36cfc7d06ff66417f23cb94f2
Alex Gleason <alex@alexgleason.me>
2018-08-29 16:34:38 -0400

    Upgrade dependencies

--- project_tier/wsgi.py
+++ project_tier/wsgi.py
@@ -10,8 +10,6 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_tier.settings.production")
 
-from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
