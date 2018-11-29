77606ad119485e16f83d834ff5e16dadea7c611f
Alex Gleason <alex@alexgleason.me>
2018-11-29 15:14:33 -0500

    Make all editor features work

--- draftailmodal/forms.py
+++ draftailmodal/forms.py
@@ -6,7 +6,9 @@ class TipForm(forms.Form):
     """
     Chooser form that appears in Draftail on the admin screen.
     """
-    tip_content = forms.CharField(widget=get_rich_text_editor_widget(
-        # features=['h2', 'h3', 'bold', 'italic', 'ol', 'ul', 'link', 'image', 'embed']
-        features=[]
-    ))
+    tip_content = forms.CharField()
+    def __init__(self, *args, **kwargs):
+        super(TipForm, self).__init__(*args, **kwargs)
+        self.fields['tip_content'].widget=get_rich_text_editor_widget(
+            features=['h2', 'h3', 'bold', 'italic', 'ol', 'ul', 'link', 'image', 'embed']
+        )
