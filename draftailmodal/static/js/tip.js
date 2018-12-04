2c3564bf0a13ca42ad0e703a1031ea36d2f15998
Alex Gleason <alex@alexgleason.me>
2018-12-04 18:35:43 -0500

    Add comments

--- draftailmodal/static/js/tip.js
+++ draftailmodal/static/js/tip.js
@@ -1,8 +1,16 @@
+// Modal plugin for Draftail.
+//
+// Extending the Draftail Editor:
+// https://docs.wagtail.io/en/latest/advanced_topics/customisation/extending_draftail.html
+
 const React = window.React;
 const Modifier = window.DraftJS.Modifier;
 const EditorState = window.DraftJS.EditorState;
 
 
+// When the ModalWorkflow endpoint responds, it'll tell us which one of
+// these functions to run (by key name). Undocumented Wagtail stuff.
+// See views.py for the endpoint code.
 const TIP_MODAL_ONLOAD_HANDLERS = {
   "chooser": function(modal, jsonData) {
     $('form.draftailmodal').on('submit', function() {
@@ -17,10 +25,9 @@ const TIP_MODAL_ONLOAD_HANDLERS = {
 }
 
 
-/**
-* Returns collection of currently selected blocks.
-* See https://github.com/jpuri/draftjs-utils/blob/e81c0ae19c3b0fdef7e0c1b70d924398956be126/js/block.js#L19.
-*/
+// Two Wagtail util functions useful for Drafttail.
+// From here: https://github.com/wagtail/wagtail/blob/master/client/src/components/Draftail/DraftUtils.js
+// FIXME: Figure out a way to import them instead of copying?
 const getSelectedBlocksList = (editorState) => {
   const selectionState = editorState.getSelection();
   const content = editorState.getCurrentContent();
@@ -34,34 +41,29 @@ const getSelectedBlocksList = (editorState) => {
     .concat([[endKey, blockMap.get(endKey)]]);
   return blocks.toList();
 };
-
-/**
-* Returns the currently selected text in the editor.
-* See https://github.com/jpuri/draftjs-utils/blob/e81c0ae19c3b0fdef7e0c1b70d924398956be126/js/block.js#L106.
-*/
 const getSelectionText = (editorState) => {
   const selection = editorState.getSelection();
   let start = selection.getAnchorOffset();
   let end = selection.getFocusOffset();
   const selectedBlocks = getSelectedBlocksList(editorState);
-
   if (selection.getIsBackward()) {
     const temp = start;
     start = end;
     end = temp;
   }
-
   let selectedText = '';
   for (let i = 0; i < selectedBlocks.size; i += 1) {
     const blockStart = i === 0 ? start : 0;
     const blockEnd = i === (selectedBlocks.size - 1) ? end : selectedBlocks.get(i).getText().length;
     selectedText += selectedBlocks.get(i).getText().slice(blockStart, blockEnd);
   }
-
   return selectedText;
 };
 
 
+// Returns the settings object for ModalWorkflow called in TipSource.
+// This does not really need to be a separate function, but I'm too afraid
+// to refactor it now.
 function getChooserConfig(entityType, entity, selectedText) {
   if (entityType.type == 'TIP') {
     return {
@@ -73,36 +75,44 @@ function getChooserConfig(entityType, entity, selectedText) {
 }
 
 
+// The main plugin logic is here.
+// This is a React component because of reasons I don't understand.
+// It doesn't render anything, it's just a dummy that runs our code.
+// Based off ModalWorkflowSource: https://github.com/wagtail/wagtail/blob/master/client/src/components/Draftail/sources/ModalWorkflowSource.js#L133
 class TipSource extends React.Component {
+
+  // Called when an instance is created
   constructor(props) {
     super(props);
-
+    // set the value of `this`
     this.onChosen = this.onChosen.bind(this);
     this.onClose = this.onClose.bind(this);
   }
 
+  // This gets run whenever the toolbar button for this plugin is clicked.
+  // It's also run whenever props.onEdit() is called.
   componentDidMount() {
     const { onClose, entityType, entity, editorState, entityKey } = this.props;
-    const selectedText = getSelectionText(editorState);
-    const { url, onload } = getChooserConfig(entityType, entity, selectedText);
+    const selectedText = getSelectionText(editorState); // that utils function
+    const { url, onload } = getChooserConfig(entityType, entity, selectedText); // that "settings object" function
     const contentState = editorState.getCurrentContent();
     var urlParams = {};
 
+    // FIXME: try/catch because..?
     try {
       const data = contentState.getEntity(entityKey).getData();
       urlParams = {data: JSON.stringify(data['tip'])};
-    } catch(err) {
-      //
-    }
+    } catch(err) {}
 
-    $(document.body).on('hidden.bs.modal', this.onClose);
+    $(document.body).on('hidden.bs.modal', this.onClose); // FIXME: idk
 
+    // Spawn the ModalWorkflow window where the text content is set!
     this.workflow = global.ModalWorkflow({
       url,
       urlParams,
       onload,
       responses: {
-        tipChosen: this.onChosen,
+        tipChosen: this.onChosen, // calls the function below
       },
       onError: () => {
         console.log("modalworkflow error");
@@ -111,15 +121,15 @@ class TipSource extends React.Component {
     });
   }
 
+  // FIXME: idk when this is called or what exactly it does
   componentWillUnmount() {
     this.workflow = null;
-
     $(document.body).off('hidden.bs.modal', this.onClose);
   }
 
+  // Called when tip content has been entered and saved
   onChosen(data) {
     const { editorState, entityType, onComplete } = this.props;
-
     const content = editorState.getCurrentContent();
     const selection = editorState.getSelection();
 
@@ -132,30 +142,31 @@ class TipSource extends React.Component {
     // We also add some text for the entity to be activated on.
     const text = 'tip';
 
+    // FIXME: idk how this works
     const newContent = Modifier.replaceText(content, selection, text, null, entityKey);
     const nextState = EditorState.push(editorState, newContent, 'insert-characters');
 
-    // IE11 crashes when rendering the new entity in contenteditable if the modal is still open.
-    // Other browsers do not mind. This is probably a focus management problem.
-    // From the user's perspective, this is all happening too fast to notice either way.
-    this.workflow.close();
+    this.workflow.close(); // Needed for IE11, apparently
 
-    onComplete(nextState);
+    onComplete(nextState); // FIXME: idk how this works
   }
 
+  // FIXME: idk how this works
   onClose(e) {
     const { onClose } = this.props;
     e.preventDefault();
-
     onClose();
   }
 
+  // Render nothing!
   render() {
     return null;
   }
 }
 
-
+// This is a Draft.js Decorator Component
+// https://draftjs.org/docs/advanced-topics-decorators#decorator-components
+// It creates the little "tip" icon rendered in the editor.
 const Tip = (props) => {
     const { entityKey, contentState } = props;
     const data = contentState.getEntity(entityKey).getData();
@@ -164,11 +175,14 @@ const Tip = (props) => {
         role: 'button',
         className: 'Draftail-inline--TIP',
         onMouseUp: () => {
+            // When a tip is clicked, edit it.
             props.onEdit(entityKey);
         },
     }, props.children);
 };
 
+// Registers the plugin
+// FIXME: I don't know this works
 window.draftail.registerPlugin({
     type: 'TIP',
     source: TipSource,
