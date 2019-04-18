5a47629f0d2b8107352c2feced79bf5fe62af3aa
Alex Gleason <alex@alexgleason.me>
2019-04-18 17:59:33 -0400

    Remove academic_title, create secondary_job_title

--- project_tier/network/models.py
+++ project_tier/network/models.py
@@ -20,13 +20,13 @@ class Person(models.Model):
     """
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    academic_title = models.CharField(max_length=255, blank=True)
     slug = models.SlugField(
         help_text="All lowercase, hyphenated name that will appear in the URL."
                   " For example, entering norm-medeiros will result in the URL"
                   " projecttier.org/person/norm-medeiros"
     )
     main_job_title = models.CharField(max_length=255, blank=True)
+    secondary_job_title = models.CharField(max_length=255, blank=True)
     affiliation = models.CharField(max_length=255, blank=True)
     email = models.EmailField(max_length=255, blank=True)
     phone = models.CharField(max_length=255, blank=True)
@@ -88,9 +88,9 @@ class Person(models.Model):
             [
                 FieldPanel('first_name'),
                 FieldPanel('last_name'),
-                FieldPanel('academic_title'),
                 FieldPanel('affiliation'),
                 FieldPanel('main_job_title'),
+                FieldPanel('secondary_job_title'),
                 FieldPanel('slug'),
             ],
             heading="Basic Person Info"
