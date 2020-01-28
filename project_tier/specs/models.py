79c28b73b8cc8a6a01d16b54492cb77964eae3c2
Alex Gleason <alex@alexgleason.me>
2020-01-28 16:22:11 -0600

    Dynamic sidebar nav #126

--- project_tier/specs/models.py
+++ project_tier/specs/models.py
@@ -8,9 +8,14 @@ from project_tier.blocks import ContentStreamBlock
 
 class SpecsPage(Page):
     """Abstract base class inherited by other spec page types."""
-    body = StreamField(ContentStreamBlock())
+    body = StreamField(ContentStreamBlock(required=False), blank=True)
 
     content_panels = Page.content_panels + [StreamFieldPanel('body')]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['spec_root_page'] = FolderPage.objects.filter(page_ptr__in=self.get_ancestors(inclusive=True)).order_by('page_ptr__depth').first()
+        return context
 
     class Meta:
         abstract = True
@@ -18,6 +23,7 @@ class SpecsPage(Page):
 
 class FolderPage(SpecsPage):
     """Represents a folder within the project hierarchy, eg: MetaData."""
+    nav_icon = 'fa-folder-open'
     parent_page_types = ['specs.FolderPage', 'specs.SpecsLandingPage']
     subpage_types = [
         'specs.FolderPage', 'specs.FilePage', 'specs.OptionalFilePage'
@@ -26,6 +32,7 @@ class FolderPage(SpecsPage):
 
 class FilePage(SpecsPage):
     """Represents a file within the project hierarchy, eg MetaDataGuide."""
+    nav_icon = 'fa-file-text'
     parent_page_types = ['specs.FolderPage']
     subpage_types = []
 
