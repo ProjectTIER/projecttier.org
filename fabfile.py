11eec71e56248a04424f0ce198cc4ce3e2d25164
Alex Gleason <alex@alexgleason.me>
2023-01-10 12:58:21 -0600

    Remove vagrant stuff from fabfile

--- fabfile.py
+++ fabfile.py
@@ -12,19 +12,16 @@ def deploy_production():
 
 
 def pull_staging_data():
-    local('psql -d project_tier -c "ALTER USER vagrant with password \'vagrant\';"')
     local('dropdb --if-exists project_tier')
     local('heroku pg:pull DATABASE_URL project_tier --app projecttier-staging')
-    local('psql -d project_tier -c "alter role vagrant password null;"')
 
 
 def pull_production_data():
     # It will prompt you to log in to Heroku. Your Heroku user needs adequate
     # permission on the projecttier-production app.
-    local('psql -d project_tier -c "ALTER USER vagrant with password \'vagrant\';"')
     local('dropdb --if-exists project_tier')
     local('heroku pg:pull DATABASE_URL project_tier --app projecttier-production')
-    local('psql -d project_tier -c "alter role vagrant password null;"')
+
 
 def pull_production_media():
     # To use this command, you must put a .s3cfg file into the root of the
@@ -32,6 +29,7 @@ def pull_production_media():
     # `s3cmd --configure` to create it yourself.
     local('mkdir -p media')
     local('s3cmd --config=.s3cfg get s3://bucketeer-82911c16-8ccd-4854-b255-5b3ebba24d7c --recursive --skip-existing --delete-removed media')
+
 
 def sync_staging():
     # Syncs the staging database with production
